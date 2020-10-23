7895dbe03601a90afe4e75feda06370a3981faec
mateusamarall <mateus.amaral018@gmail.com>
2020-10-23 08:46:08 -0300

    first commit

---
+++ web/src/routes.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Landing from './Pages/Landing';
+import OrphanagesMap from './Pages/OrphanagesMap';
+import Orphanage from './Pages/Orphanage';
+import CreateOrphanage from './Pages/CreateOrphanage';
+
+
+
+function Routes(){
+  return(
+    <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Landing} />
+      <Route path="/app" component={OrphanagesMap} />
+      <Route path="/orphanages/create" component={CreateOrphanage} />
+      <Route path="/orphanages/:id" component={Orphanage} />
+
+
+    </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default Routes;
